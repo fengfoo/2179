9eb81e7bfd487ac6f0ae93db52a26d1d5a39cb51
Foo Jian Feng <jfoo0033@student.monash.edu>
2026-05-09 14:54:17 +0800

    Uploading main.js file

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*
+=============================================================================
+  Student Name: Foo Jian Feng
+  Student ID: 35172495
+  Assignment: FIT2179 - Data Visualisation 2
+  Description: This code is for the assignment of FIT2179. This code is 
+  NOT meant to be reused or copied in any way outside of grading purposes only.
+  Date: May 2026
+=============================================================================
+*/
+
+var map = "json/map.vg.json";
+var c2 = "json/chart2.vg.json";
+var c3 = "json/chart3.vg.json";
+var c4 = "json/chart4.vg.json";
+var c5 = "json/chart5.vg.json";
+var c6 = "json/chart6.vg.json";
+var c7 = "json/chart7.vg.json";
+var c8 = "json/chart8.vg.json";
+var c9 = "json/chart9.vg.json";
+var c10 = "json/chart10.vg.json";
+var c11 = "json/chart11.vg.json";
+
+vegaEmbed('#map_chart', map).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart2', c2).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart3', c3).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart4', c4).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart5', c5).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart6', c6).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart7', c7).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart8', c8).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart9', c9).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart10', c10).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart11', c11).then(function(result) {}).catch(console.error);
