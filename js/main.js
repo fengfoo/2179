ac734a7a929978a1790516717ba35aa851ad3c11
Foo Jian Feng <jfoo0033@student.monash.edu>
2026-05-16 14:13:24 +0800

    Adding interactivity to main.js

--- js/main.js
+++ js/main.js
@@ -10,25 +10,65 @@
 */
 
 var map = "json/map.vg.json";
-var c2 = "json/chart2.vg.json";
-var c3 = "json/chart3.vg.json";
-var c4 = "json/chart4.vg.json";
-var c5 = "json/chart5.vg.json";
-var c6 = "json/chart6.vg.json";
-var c7 = "json/chart7.vg.json";
-var c8 = "json/chart8.vg.json";
-var c9 = "json/chart9.vg.json";
+var c2  = "json/chart2.vg.json";
+var c3  = "json/chart3.vg.json";
+var c4  = "json/chart4.vg.json";
+var c5  = "json/chart5.vg.json";
+var c6  = "json/chart6.vg.json";
+var c7  = "json/chart7.vg.json";
+var c8  = "json/chart8.vg.json";
+var c9  = "json/chart9.vg.json";
 var c10 = "json/chart10.vg.json";
 var c11 = "json/chart11.vg.json";
 
-vegaEmbed('#map_chart', map).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart2', c2).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart3', c3).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart4', c4).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart5', c5).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart6', c6).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart7', c7).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart8', c8).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart9', c9).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart10', c10).then(function(result) {}).catch(console.error);
-vegaEmbed('#chart11', c11).then(function(result) {}).catch(console.error);
+// For chart connections to have state focus in Section 4
+var selectedState = null;
+
+var mapView    = null;
+var chart3View = null;
+var chart5View = null;
+
+function broadcastState(stateName) {
+  selectedState = stateName;
+  if (mapView)    mapView.signal('ext_state', stateName).run();
+  if (chart3View) chart3View.signal('ext_state', stateName).run();
+  if (chart5View) chart5View.signal('ext_state', stateName).run();
+}
+
+vegaEmbed('#map_chart', map, { actions: false }).then(function(result) {
+  mapView = result.view;
+
+
+  mapView.addEventListener('click', function(event, item) {
+    if (item && item.datum && item.datum.state) {
+      var clicked = item.datum.state;
+
+      if (selectedState === clicked) {
+        broadcastState(null);
+      } else {
+        broadcastState(clicked);
+      }
+    } else {
+      // Clicking ocean/state again clear everything
+      broadcastState(null);
+    }
+  });
+
+}).catch(console.error);
+
+vegaEmbed('#chart3', c3, { actions: false }).then(function(result) {
+  chart3View = result.view;
+}).catch(console.error);
+
+vegaEmbed('#chart5', c5, { actions: false }).then(function(result) {
+  chart5View = result.view;
+}).catch(console.error);
+
+vegaEmbed('#chart2',  c2,  { actions: false }).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart4',  c4,  { actions: false }).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart6',  c6,  { actions: false }).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart7',  c7,  { actions: false }).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart8',  c8,  { actions: false }).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart9',  c9,  { actions: false }).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart10', c10, { actions: false }).then(function(result) {}).catch(console.error);
+vegaEmbed('#chart11', c11, { actions: false }).then(function(result) {}).catch(console.error);
